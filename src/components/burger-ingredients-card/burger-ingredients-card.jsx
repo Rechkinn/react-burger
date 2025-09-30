5ee84efe4f55bf4d544e0d83aa1b5e2956cf4360
Rechkinn <al.red197218@gmail.com>
2025-10-01 00:17:37 +0700

    Компонент BurgerIngredients переписан на функциональный компонент

--- src/components/burger-ingredients-card/burger-ingredients-card.jsx
+++ src/components/burger-ingredients-card/burger-ingredients-card.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import styles from "./burger-ingredients-card.module.css";
 import {
   Counter,
@@ -8,55 +8,46 @@ import {
 import PropTypes from "prop-types";
 import { IngredientType } from "../../utils/types";
 
-class BurgerIngredientsCard extends React.Component {
-  state = {
-    currentDevice: window.innerWidth > 768 ? "notMobile" : "mobile",
-  };
+function BurgerIngredientsCard({ ingredient }) {
+  const [currentDevice, setCurrentDevice] = useState(
+    window.innerWidth > 768 ? "notMobile" : "mobile"
+  );
 
-  updateImageCard = () => {
-    if (window.innerWidth > 768) {
-      this.setState({ currentDevice: "notMobile" });
-    } else {
-      this.setState({ currentDevice: "mobile" });
-    }
-  };
-
-  componentDidMount() {
-    window.addEventListener("resize", this.updateImageCard);
+  function updateImageCard() {
+    window.innerWidth > 768
+      ? setCurrentDevice("notMobile")
+      : setCurrentDevice("mobile");
   }
 
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateImageCard);
-  }
+  useEffect(() => {
+    window.addEventListener("resize", updateImageCard);
+    return () => window.removeEventListener("resize", updateImageCard);
+  }, []);
 
-  render() {
-    return (
-      <article className={`mb-8 ${styles.card}`}>
-        <img
-          src={
-            this.state.currentDevice === "mobile"
-              ? this.props.ingredient.image_mobile
-              : this.props.ingredient.image
-          }
-          alt={this.props.ingredient.name}
-          className={`mb-1 ${styles.image}`}
-        />
-        <p className={`mb-1 ${styles.price}`}>
-          <span className="mr-2 text text_type_digits-default">
-            {this.props.ingredient.price}
-          </span>
-          <CurrencyIcon />
-        </p>
-        <h3 className="mb-5 text text_type_main-small">
-          {this.props.ingredient.name}
-        </h3>
-        <Button htmlType="button" type="secondary" size="small">
-          Добавить
-        </Button>
-        <Counter />
-      </article>
-    );
-  }
+  return (
+    <article className={`mb-8 ${styles.card}`}>
+      <img
+        src={
+          currentDevice === "mobile"
+            ? ingredient.image_mobile
+            : ingredient.image
+        }
+        alt={ingredient.name}
+        className={`mb-1 ${styles.image}`}
+      />
+      <p className={`mb-1 ${styles.price}`}>
+        <span className="mr-2 text text_type_digits-default">
+          {ingredient.price}
+        </span>
+        <CurrencyIcon />
+      </p>
+      <h3 className="mb-5 text text_type_main-small">{ingredient.name}</h3>
+      <Button htmlType="button" type="secondary" size="small">
+        Добавить
+      </Button>
+      <Counter />
+    </article>
+  );
 }
 
 export default BurgerIngredientsCard;
