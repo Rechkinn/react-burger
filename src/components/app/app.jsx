b0a1fa35d850eca22ba163ae1b0417b7db604805
Rechkinn <al.red197218@gmail.com>
2025-10-01 01:46:16 +0700

    Подключитение к API

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,42 +1,81 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./app.module.css";
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import { data } from "../../utils/data";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 
 function App() {
-  const [activeSection, setActiveSection] = useState("BurgerIngredients");
+  const [state, setState] = useState({
+    activeSection: "BurgerIngredients",
+    loading: true,
+    hasError: false,
+    data: [],
+  });
+
+  useEffect(() => {
+    const getIngredients = async () => {
+      const url = `https://norma.nomoreparties.space/api/ingredients`;
+      fetch(url)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) =>
+          setState({
+            ...state,
+            data: data.data,
+            loading: false,
+            hasError: false,
+          })
+        )
+        .catch((error) =>
+          setState({ ...state, hasError: error, loading: false })
+        );
+    };
+
+    getIngredients();
+  }, []);
 
   return (
     <>
-      <AppHeader />
-      <main className={styles.main}>
-        <section
-          className={
-            activeSection !== "BurgerIngredients"
-              ? `pt-10 ${styles.sectionIngredients} ${styles.hidden1280}`
-              : `pt-10 ${styles.sectionIngredients}`
-          }
-        >
-          <BurgerIngredients
-            arrayOfIngredients={data}
-            openBurgerConstructor={() => setActiveSection("BurgerConstructor")}
-          />
-        </section>
-        <section
-          className={
-            activeSection !== "BurgerConstructor"
-              ? `pl-4 pr-4 ${styles.sectionConstructor} ${styles.hidden1280}`
-              : `pl-4 pr-4 ${styles.sectionConstructor}`
-          }
-        >
-          <BurgerConstructor
-            arrayOfIngredients={data}
-            closeBurgerConstructor={() => setActiveSection("BurgerIngredients")}
-          />
-        </section>
-      </main>
+      {state.loading && <div>LOADING...</div>}
+      {!state.loading && state.hasError && (
+        <div>Error: {state.hasError.message}</div>
+      )}
+      {!state.loading && !state.hasError && (
+        <>
+          <AppHeader />
+          <main className={styles.main}>
+            <section
+              className={
+                state.activeSection !== "BurgerIngredients"
+                  ? `pt-10 ${styles.sectionIngredients} ${styles.hidden1280}`
+                  : `pt-10 ${styles.sectionIngredients}`
+              }
+            >
+              <BurgerIngredients
+                arrayOfIngredients={state.data}
+                openBurgerConstructor={() =>
+                  setState({ ...state, activeSection: "BurgerConstructor" })
+                }
+              />
+            </section>
+            <section
+              className={
+                state.activeSection !== "BurgerConstructor"
+                  ? `pl-4 pr-4 ${styles.sectionConstructor} ${styles.hidden1280}`
+                  : `pl-4 pr-4 ${styles.sectionConstructor}`
+              }
+            >
+              <BurgerConstructor
+                arrayOfIngredients={state.data}
+                closeBurgerConstructor={() =>
+                  setState({ ...state, activeSection: "BurgerIngredients" })
+                }
+              />
+            </section>
+          </main>
+        </>
+      )}
     </>
   );
 }
