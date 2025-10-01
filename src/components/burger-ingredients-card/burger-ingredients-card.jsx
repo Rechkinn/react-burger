dfbfc76ca8182c2cf4b193c5ae60982665e01155
Rechkinn <al.red197218@gmail.com>
2025-10-02 00:49:52 +0700

    Добавил открытие модального окна при клике по ингредиенту в секции конструктора.

--- src/components/burger-ingredients-card/burger-ingredients-card.jsx
+++ src/components/burger-ingredients-card/burger-ingredients-card.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import styles from "./burger-ingredients-card.module.css";
 import {
   Counter,
@@ -7,11 +7,21 @@ import {
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import PropTypes from "prop-types";
 import { IngredientType } from "../../utils/types";
+import Modal from "../modal/modal";
+import IngredientDetails from "../ingredient-details/ingredient-details";
 
 function BurgerIngredientsCard({ ingredient }) {
   const [currentDevice, setCurrentDevice] = useState(
     window.innerWidth > 768 ? "notMobile" : "mobile"
   );
+  const [isOpeningModal, setIsOpeningModal] = useState(false);
+
+  const openModal = () => {
+    setIsOpeningModal(true);
+  };
+  const closeModal = () => {
+    setIsOpeningModal(false);
+  };
 
   function updateImageCard() {
     window.innerWidth > 768
@@ -25,28 +35,35 @@ function BurgerIngredientsCard({ ingredient }) {
   }, []);
 
   return (
-    <article className={`mb-8 ${styles.card}`}>
-      <img
-        src={
-          currentDevice === "mobile"
-            ? ingredient.image_mobile
-            : ingredient.image
-        }
-        alt={ingredient.name}
-        className={`mb-1 ${styles.image}`}
-      />
-      <p className={`mb-1 ${styles.price}`}>
-        <span className="mr-2 text text_type_digits-default">
-          {ingredient.price}
-        </span>
-        <CurrencyIcon />
-      </p>
-      <h3 className="mb-5 text text_type_main-small">{ingredient.name}</h3>
-      <Button htmlType="button" type="secondary" size="small">
-        Добавить
-      </Button>
-      <Counter />
-    </article>
+    <>
+      {isOpeningModal && (
+        <Modal type={"IngredientDetails"} functionToClose={closeModal}>
+          <IngredientDetails ingredient={ingredient} />
+        </Modal>
+      )}
+      <article className={`mb-8 ${styles.card}`} onClick={openModal}>
+        <img
+          src={
+            currentDevice === "mobile"
+              ? ingredient.image_mobile
+              : ingredient.image
+          }
+          alt={ingredient.name}
+          className={`mb-1 ${styles.image}`}
+        />
+        <p className={`mb-1 ${styles.price}`}>
+          <span className="mr-2 text text_type_digits-default">
+            {ingredient.price}
+          </span>
+          <CurrencyIcon />
+        </p>
+        <h3 className="mb-5 text text_type_main-small">{ingredient.name}</h3>
+        <Button htmlType="button" type="secondary" size="small">
+          Добавить
+        </Button>
+        <Counter />
+      </article>
+    </>
   );
 }
 
