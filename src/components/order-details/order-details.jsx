9f5162624ed49105ef1de9bfda2e35f3d0ae5c39
Aleksey Redkin <al.red197218@gmail.com>
2025-10-13 14:29:38 +0700

    Добавление логики создания заказ через редакс

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,25 +1,56 @@
 import { CheckMarkIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./order-details.module.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createNewOrder } from "../../services/actions/order-details";
 
 function OrderDetails() {
+  const { burgerConstructor } = useSelector((store) => store.burgerConstructor);
+
+  const dispatch = useDispatch();
+
+  const { orderDetails, orderDetailsRequest, orderDetailsRequestFailed } =
+    useSelector((store) => store.orderDetails);
+
+  console.log("orderDetails", orderDetails);
+
+  useEffect(() => {
+    dispatch(createNewOrder(getIngredientsIds()));
+  }, []);
+
+  function getIngredientsIds() {
+    return [...burgerConstructor.filter((ingredient) => ingredient._id)];
+  }
+
   return (
-    <div className={`pb-20 ${styles.orderDetails}`}>
-      <h1 className={`text text_type_digits-large mb-8 ${styles.id}`}>
-        034536
-      </h1>
-      <p className="text text_type_main-medium mb-15">идентификатор заказа</p>
+    <>
+      {orderDetailsRequest && <div>Загрузка...</div>}
+      {!orderDetailsRequest && orderDetailsRequestFailed && (
+        <div>Ошибка создания заказа!</div>
+      )}
 
-      <div className={`mb-15 ${styles.backgroundCheckMarkIcon}`}>
-        <CheckMarkIcon />
-      </div>
+      {!orderDetailsRequest && !orderDetailsRequestFailed && (
+        <div className={`pb-20 ${styles.orderDetails}`}>
+          <h1 className={`text text_type_digits-large mb-8 ${styles.id}`}>
+            {orderDetails?.order?.number}
+          </h1>
+          <p className="text text_type_main-medium mb-15">
+            идентификатор заказа
+          </p>
 
-      <p className="text mb-2 text_type_main-small">
-        Ваш заказ начали готовить
-      </p>
-      <p className="text text_type_main-small text_color_inactive">
-        Дождитесь готовности на орбитальной станции
-      </p>
-    </div>
+          <div className={`mb-15 ${styles.backgroundCheckMarkIcon}`}>
+            <CheckMarkIcon />
+          </div>
+
+          <p className="text mb-2 text_type_main-small">
+            Ваш заказ начали готовить
+          </p>
+          <p className="text text_type_main-small text_color_inactive">
+            Дождитесь готовности на орбитальной станции
+          </p>
+        </div>
+      )}
+    </>
   );
 }
 
