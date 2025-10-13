179a20793ab7951eef6d2ee97d200b93cb114658
Rechkinn <al.red197218@gmail.com>
2025-10-14 00:54:42 +0700

    Добавление логики перемещения ингредиента в конструкторе. Изменение логики удаление ингредиента в конструкторе

--- src/services/reducers/burger-constructor.js
+++ src/services/reducers/burger-constructor.js
@@ -2,6 +2,7 @@ import {
   ADD_INGREDIENT_TO_CONSTRUCTOR,
   REMOVE_INGREDIENT_FROM_CONSTRUCTOR,
   SET_BUN,
+  CHANGE_SUBSEQUENCE_BURGER_CONSTRUCTOR,
 } from "../actions/burger-constructor";
 
 const initialState = {
@@ -22,14 +23,58 @@ export const burderConstructorReducer = (state = initialState, action) => {
         burgerConstructor: [...state.burgerConstructor, action.ingredient],
       };
     case REMOVE_INGREDIENT_FROM_CONSTRUCTOR:
+      let hasAlreadyRemoved = false;
+
       return {
         ...state,
         burgerConstructor: [
           ...state.burgerConstructor.filter((ingredient) => {
-            if (ingredient._id !== action.id) return ingredient;
+            if (
+              ingredient.ingredient._id !== action.id ||
+              hasAlreadyRemoved === true
+            ) {
+              return ingredient.ingredient;
+            } else {
+              hasAlreadyRemoved = true;
+            }
           }),
         ],
       };
+    case CHANGE_SUBSEQUENCE_BURGER_CONSTRUCTOR:
+      const positionDrag = action.positionDragElement.positionList;
+      const positionDropTarget = action.positionDropTargetElement;
+
+      if (positionDrag === positionDropTarget) return state;
+      else if (positionDrag < positionDropTarget) {
+        return {
+          ...state,
+          burgerConstructor: [
+            ...state.burgerConstructor.slice(0, positionDrag),
+            ...state.burgerConstructor.slice(
+              positionDrag + 1,
+              positionDropTarget
+            ),
+            state.burgerConstructor[positionDrag],
+            ...state.burgerConstructor.slice(
+              positionDropTarget,
+              state.burgerConstructor.length
+            ),
+          ],
+        };
+      } else {
+        return {
+          ...state,
+          burgerConstructor: [
+            ...state.burgerConstructor.slice(0, positionDropTarget),
+            state.burgerConstructor[positionDrag],
+            ...state.burgerConstructor.slice(positionDropTarget, positionDrag),
+            ...state.burgerConstructor.slice(
+              positionDrag + 1,
+              state.burgerConstructor.length
+            ),
+          ],
+        };
+      }
     default:
       return state;
   }
