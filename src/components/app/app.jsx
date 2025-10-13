6ed663de3cb769ac64ee16e23685a2f1ca062d69
Aleksey Redkin <al.red197218@gmail.com>
2025-10-13 14:38:37 +0700

    Добавление логики получения всех ингредиентов через редакс. Убраны не нужные пропсы, передаваемые в другие компоненты.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,82 +3,69 @@ import styles from "./app.module.css";
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
+import { useDispatch, useSelector } from "react-redux";
 
-const url = `https://norma.nomoreparties.space`;
+import { getBurgerIngredients } from "../../services/actions/burger-ingredients";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { BUN } from "../../utils/consts";
+import { ADD_INGREDIENT_TO_CONSTRUCTOR } from "../../services/actions/burger-constructor";
 
 function App() {
-  const [activeSection, setActiveSection] = useState("BurgerConstructor");
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState(null);
+  // const [activeSection, setActiveSection] = useState("BurgerConstructor");
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [error, setError] = useState(null);
+  // const [data, setData] = useState(null);
+
+  const {
+    burgerIngredients,
+    burgerIngredientsRequest,
+    burgerIngredientsRequestFailed,
+  } = useSelector((store) => store.burgerIngredients);
+
+  const { activeSection } = useSelector((store) => store.activeSection);
+  const { burgerConstructor } = useSelector((store) => store.burgerConstructor);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const controller = new AbortController();
-
-    const getIngredients = async () => {
-      setIsLoading(true);
-      setError(null);
-
-      try {
-        const response = await fetch(`${url}/api/ingredients`, {
-          signal: controller.signal,
-        });
-        if (!response.ok) throw new Error(`Ошибка: ${response.status}`);
-        const result = await response.json();
-        setData(result.data);
-      } catch (err) {
-        if (err.name !== "AbortError") {
-          setError(err.message);
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    getIngredients();
-
-    return () => controller.abort();
+    dispatch(getBurgerIngredients());
   }, []);
 
   return (
     <>
-      {isLoading && <div className="text text_type_main-large">LOADING...</div>}
-      {error && (
-        <div className="text text_type_main-large">Error: {error.message}</div>
+      {burgerIngredientsRequest && (
+        <div className="text text_type_main-large">LOADING...</div>
       )}
-      {data && (
+      {!burgerIngredientsRequest && burgerIngredientsRequestFailed && (
+        <div className="text text_type_main-large">
+          Ошибка получения данных об ингредиентах!
+        </div>
+      )}
+      {!burgerIngredientsRequest && !burgerIngredientsRequestFailed && (
         <>
           <AppHeader />
           <main className={styles.main}>
-            <section
-              className={
-                activeSection !== "BurgerIngredients"
-                  ? `pt-10 ${styles.sectionIngredients} ${styles.hidden1280}`
-                  : `pt-10 ${styles.sectionIngredients}`
-              }
-            >
-              <BurgerIngredients
-                arrayOfIngredients={data}
-                objectToOpenSectionBurgerConstructor={{
-                  currentSection: activeSection,
-                  func: () => setActiveSection("BurgerConstructor"),
-                }}
-              />
-            </section>
-            <section
-              className={
-                activeSection !== "BurgerConstructor"
-                  ? `pl-4 pr-4 ${styles.sectionConstructor} ${styles.hidden1280}`
-                  : `pl-4 pr-4 ${styles.sectionConstructor}`
-              }
-            >
-              <BurgerConstructor
-                arrayOfIngredients={data}
-                closeBurgerConstructor={() =>
-                  setActiveSection("BurgerIngredients")
+            <DndProvider backend={HTML5Backend}>
+              <section
+                className={
+                  activeSection !== "BurgerIngredients"
+                    ? `pt-10 ${styles.sectionIngredients} ${styles.hidden1280}`
+                    : `pt-10 ${styles.sectionIngredients}`
                 }
-              />
-            </section>
+              >
+                <BurgerIngredients />
+              </section>
+              <section
+                className={
+                  activeSection !== "BurgerConstructor"
+                    ? `pl-4 pr-4 ${styles.sectionConstructor} ${styles.hidden1280}`
+                    : `pl-4 pr-4 ${styles.sectionConstructor}`
+                }
+              >
+                <BurgerConstructor />
+              </section>
+            </DndProvider>
           </main>
         </>
       )}
