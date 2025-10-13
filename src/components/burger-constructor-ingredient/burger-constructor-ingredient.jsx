49b2c3ea5c74d00cbd00a9971ae5dc23279d2165
Rechkinn <al.red197218@gmail.com>
2025-10-14 00:59:29 +0700

    Добавление дополнительного контейнера div для создания логики перемещения ингредиентов в конструкторе.

--- src/components/burger-constructor-ingredient/burger-constructor-ingredient.jsx
+++ src/components/burger-constructor-ingredient/burger-constructor-ingredient.jsx
@@ -7,41 +7,90 @@ import {
 import PropTypes from "prop-types";
 import { IngredientType } from "../../utils/types";
 import { BUN } from "../../utils/consts";
-import { useDrop } from "react-dnd";
+import { useDrag, useDrop } from "react-dnd";
+import { useDispatch } from "react-redux";
+import {
+  CHANGE_SUBSEQUENCE_BURGER_CONSTRUCTOR,
+  REMOVE_INGREDIENT_FROM_CONSTRUCTOR,
+} from "../../services/actions/burger-constructor";
 
 function BurgerConstructorIngredient({
+  positionList,
   ingredient,
   indents,
   isDesctop,
   ...props
 }) {
+  const dispatch = useDispatch();
+  const [{ isDrag }, dragRef] = useDrag({
+    type: "constructorElement",
+
+    item: { positionList },
+    collect: (monitor) => ({
+      isDrag: monitor.isDragging(),
+    }),
+  });
+  const [{ isHover }, dropTarget] = useDrop({
+    accept: "constructorElement",
+    drop(positionIngredientFromDrag) {
+      dispatch({
+        type: CHANGE_SUBSEQUENCE_BURGER_CONSTRUCTOR,
+        positionDropTargetElement: positionList,
+        positionDragElement: positionIngredientFromDrag,
+      });
+    },
+    collect: (monitor) => ({
+      isHover: monitor.isOver(),
+    }),
+  });
+
   function isBun() {
     return ingredient.type === BUN;
+  }
+  function removeBurgerConstructorIngredient(ingredientId) {
+    dispatch({
+      type: REMOVE_INGREDIENT_FROM_CONSTRUCTOR,
+      id: ingredientId,
+    });
   }
 
   return (
     <>
       {ingredient && (
-        <article className={`${indents} ${styles.ingredient}`}>
-          {/* {console.log("ingredient --- ", ingredient)} */}
-          {!isBun() && <DragIcon />}
-          {isDesctop ? (
-            <ConstructorElement
-              type={props?.typeBun}
-              isLocked={isBun()}
-              text={ingredient.name}
-              price={ingredient.price}
-              thumbnail={ingredient.image_mobile}
-            />
-          ) : (
-            <ConstructorElementCustom
-              isLocked={isBun()}
-              text={ingredient.name}
-              price={ingredient.price}
-              thumbnail={ingredient.image_mobile}
-            />
-          )}
-        </article>
+        <div
+          ref={ingredient.type !== BUN ? dropTarget : null}
+          style={{
+            backgroundColor: isHover ? "#4c4cff" : "transparent",
+            borderRadius: "500px",
+          }}
+        >
+          <article
+            ref={ingredient.type !== BUN ? dragRef : null}
+            style={{ opacity: isDrag ? 0.3 : 1 }}
+            className={`${indents} ${styles.ingredient}`}
+          >
+            {!isBun() && <DragIcon />}
+            {isDesctop ? (
+              <ConstructorElement
+                type={props?.typeBun}
+                isLocked={isBun()}
+                text={ingredient.name}
+                price={ingredient.price}
+                thumbnail={ingredient.image_mobile}
+                handleClose={() =>
+                  removeBurgerConstructorIngredient(ingredient._id)
+                }
+              />
+            ) : (
+              <ConstructorElementCustom
+                isLocked={isBun()}
+                text={ingredient.name}
+                price={ingredient.price}
+                thumbnail={ingredient.image_mobile}
+              />
+            )}
+          </article>
+        </div>
       )}
     </>
   );
@@ -50,8 +99,9 @@ function BurgerConstructorIngredient({
 export default BurgerConstructorIngredient;
 
 BurgerConstructorIngredient.propTypes = {
-  isDesctop: PropTypes.bool.isRequired,
+  positionList: PropTypes.number.isRequired,
   ingredient: IngredientType.isRequired,
-  typeBun: PropTypes.string,
   indents: PropTypes.string.isRequired,
+  isDesctop: PropTypes.bool.isRequired,
+  typeBun: PropTypes.string,
 };
