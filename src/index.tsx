de750481e09eca113e135aa28e1531ae5fbaa2f2
Aleksey Redkin <al.red197218@gmail.com>
2025-10-13 14:39:14 +0700

    Добавление логики создания хранилища и его поключения к приложению

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
-import App from "./components/app/app";
+import App from "./components/app/app.jsx";
+
+// import { thunk } from "redux-thunk";
+import { rootReducer } from "./services/reducers/index.js";
+import { Provider } from "react-redux";
+
+// import { compose, applyMiddleware } from "redux";
+
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  devTools: process.env.NODE_ENV !== "production", // включено по-умолчанию
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
