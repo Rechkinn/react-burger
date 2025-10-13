200181a43623b9317275cc85169bb4c033b9ff0e
Rechkinn <al.red197218@gmail.com>
2025-10-14 00:51:46 +0700

    Удаление не существующих редьюсеров

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -3,14 +3,10 @@ import { burgerIngredientsReducer } from "./burger-ingredients";
 import { ingredientDetailsReducer } from "./ingredient-details";
 import { orderDetailsReducer } from "./order-details";
 import { burderConstructorReducer } from "./burger-constructor";
-import { activeSectionReduce } from "./active-section";
-import { modalReducer } from "./modal";
 
 export const rootReducer = combineReducers({
   burgerIngredients: burgerIngredientsReducer,
   burgerConstructor: burderConstructorReducer,
   ingredientDetails: ingredientDetailsReducer,
   orderDetails: orderDetailsReducer,
-  activeSection: activeSectionReduce,
-  modal: modalReducer,
 });
