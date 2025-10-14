fee2fc9c0b2b77065bb728f8a3c83d864d8a2c13
Aleksey Redkin <al.red197218@gmail.com>
2025-10-14 14:30:43 +0700

    Создание action creator при добавление ингредиента в конструктор

--- src/services/actions/burger-constructor.js
+++ src/services/actions/burger-constructor.js
@@ -1,6 +1,19 @@
+// import { uuid4 } from "uuid";
+import { v4 as uuid } from "uuid";
+
 export const ADD_INGREDIENT_TO_CONSTRUCTOR = "ADD_INGREDIENT_TO_CONSTRUCTOR";
 export const REMOVE_INGREDIENT_FROM_CONSTRUCTOR =
   "REMOVE_INGREDIENT_FROM_CONSTRUCTOR";
 export const SET_BUN = "SET_BUN";
 export const CHANGE_SUBSEQUENCE_BURGER_CONSTRUCTOR =
   "CHANGE_SUBSEQUENCE_BURGER_CONSTRUCTOR";
+
+export const addIngridient = (item) => {
+  return {
+    type: ADD_INGREDIENT_TO_CONSTRUCTOR,
+    payload: {
+      ...item,
+      uniqueId: uuid(),
+    },
+  };
+};
