0fb1b54fe1cf55f692228aa80a76b8c693e6a201
Aleksey Redkin <al.red197218@gmail.com>
2025-10-24 21:55:17 +0700

    Создание компонента для регистрации

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,5 +1,46 @@
+import {
+  Button,
+  Input,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./register.module.css";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
 export default function Register() {
-  return <></>;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const navigate = useNavigate();
+
+  return (
+    <main className={styles.main}>
+      <h1 className="text text_type_main-medium">Регистрация</h1>
+      <Input placeholder="Имя" type="text" extraClass={`${styles.input}`} />
+      <Input placeholder="E-mail" type="text" extraClass={`${styles.input}`} />
+      <Input
+        extraClass={`${styles.input}`}
+        placeholder="Пароль"
+        type={!showPassword ? "password" : "text"}
+        icon={!showPassword ? "ShowIcon" : "HideIcon"}
+        onIconClick={() => setShowPassword(!showPassword)}
+      />
+      <Button htmlType="button" type="primary" size="medium" extraClass="mt-6">
+        Зарегистрироваться
+      </Button>
+      <p className="mt-20">
+        <span className="text text_type_main-default">
+          Уже зарегистрированы?
+        </span>
+        <Button
+          htmlType="button"
+          type="secondary"
+          size="medium"
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
+          Войти
+        </Button>
+      </p>
+    </main>
+  );
 }
