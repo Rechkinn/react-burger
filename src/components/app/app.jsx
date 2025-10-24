1f778c33bc992982953615fee79608c39feb0397
Rechkinn <al.red197218@gmail.com>
2025-10-24 20:49:37 +0700

    Изменение структуры кода в компоненте App и добавление некотоых путей дял маршрутизации

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,21 +1,30 @@
-import { useEffect, useState } from "react";
-import styles from "./app.module.css";
+import { useEffect } from "react";
 import AppHeader from "../app-header/app-header";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
 import { useDispatch, useSelector } from "react-redux";
 import { getBurgerIngredients } from "../../services/actions/burger-ingredients";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router";
+import Main from "../../pages/main/main";
+import Login from "../../pages/login/login";
+import Register from "../../pages/register/register";
 
 function App() {
-  const [activeSection, setActiveSection] = useState("BurgerConstructor");
   const { burgerIngredientsRequest, burgerIngredientsRequestFailed } =
     useSelector((store) => store.burgerIngredients);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getBurgerIngredients());
   }, []);
+
+  // const navigate = useNavigate();
+  // if (true) {
+  //   navigate("/login");
+  // }
 
   return (
     <>
@@ -30,32 +39,19 @@ function App() {
       {!burgerIngredientsRequest && !burgerIngredientsRequestFailed && (
         <>
           <AppHeader />
-          <main className={styles.main}>
-            <DndProvider backend={HTML5Backend}>
-              <section
-                className={
-                  activeSection !== "BurgerIngredients"
-                    ? `pt-10 ${styles.sectionIngredients} ${styles.hidden1280}`
-                    : `pt-10 ${styles.sectionIngredients}`
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route
+                path="*"
+                element={
+                  <div className="text text_type_main-large">NOT FOUND 404</div>
                 }
-              >
-                <BurgerIngredients />
-              </section>
-              <section
-                className={
-                  activeSection !== "BurgerConstructor"
-                    ? `pl-4 pr-4 ${styles.sectionConstructor} ${styles.hidden1280}`
-                    : `pl-4 pr-4 ${styles.sectionConstructor}`
-                }
-              >
-                <BurgerConstructor
-                  closeBurgerConstructor={() =>
-                    setActiveSection("BurgerIngredients")
-                  }
-                />
-              </section>
-            </DndProvider>
-          </main>
+              />
+            </Routes>
+          </BrowserRouter>
         </>
       )}
     </>
