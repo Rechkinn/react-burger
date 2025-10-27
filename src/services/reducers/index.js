1097f504695eaee6b2e00de2d3d099a94e2ce2a5
Aleksey Redkin <al.red197218@gmail.com>
2025-10-27 17:47:25 +0700

    Подключение всех необходимых редьюсеров в корневой редьюсер

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -3,10 +3,24 @@ import { burgerIngredientsReducer } from "./burger-ingredients";
 import { ingredientDetailsReducer } from "./ingredient-details";
 import { orderDetailsReducer } from "./order-details";
 import { burderConstructorReducer } from "./burger-constructor";
+import { userDataReducer } from "./user-data";
+import { registerReducer } from "./register";
+import { tokenReducer } from "./token";
+import { loginReducer } from "./login";
+import { logoutReducer } from "./logout";
+import { forgotPasswordReducer } from "./forgot-password";
+import { resetPasswordReducer } from "./reset-password";
 
 export const rootReducer = combineReducers({
   burgerIngredients: burgerIngredientsReducer,
   burgerConstructor: burderConstructorReducer,
   ingredientDetails: ingredientDetailsReducer,
   orderDetails: orderDetailsReducer,
+  userData: userDataReducer,
+  register: registerReducer,
+  token: tokenReducer,
+  login: loginReducer,
+  logout: logoutReducer,
+  forgotPassword: forgotPasswordReducer,
+  resetPassword: resetPasswordReducer,
 });
