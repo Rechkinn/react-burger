2d2a05865bad94dc893a7650bc6edc39c1cbf772
Aleksey Redkin <al.red197218@gmail.com>
2025-10-27 17:40:50 +0700

    Добавлена функция для авторизации. Добавлена логика перенаправления. Добавление вещей для работы с формой.

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -3,28 +3,76 @@ import {
   Input,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./login.module.css";
-import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useRef, useState } from "react";
+import { Navigate, useNavigate } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { doLogin } from "../../services/actions/login";
+import { collectUserData } from "../../utils/collectUserData";
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
 
+  const { user } = useSelector((store) => store.userData);
+  const { loginRequest, loginRequestError } = useSelector(
+    (store) => store.login
+  );
+
   const navigate = useNavigate();
+
+  const formRef = useRef();
+
+  const dispatch = useDispatch();
+  function login(e) {
+    e.preventDefault();
+    dispatch(doLogin(collectUserData(formRef.current.elements)));
+  }
+
+  const [inputEmailValue, setInputEmailValue] = useState(
+    "al.red197218@gmail.com"
+  );
+  const [inputPasswordValue, setInputPasswordValue] = useState("qwerty");
+
+  if (user) {
+    return <Navigate to="/" replace />;
+  }
 
   return (
     <main className={styles.main}>
+      {loginRequest && <div>Авторизация пользователя...</div>}
+      {loginRequestError && <div>Ошибка авторизации.</div>}
       <h1 className="text text_type_main-medium">Вход</h1>
-      <Input placeholder="E-mail" type="text" extraClass={`${styles.input}`} />
-      <Input
-        extraClass={`${styles.input}`}
-        placeholder="Пароль"
-        type={!showPassword ? "password" : "text"}
-        icon={!showPassword ? "ShowIcon" : "HideIcon"}
-        onIconClick={() => setShowPassword(!showPassword)}
-      />
-      <Button htmlType="button" type="primary" size="medium" extraClass="mt-6">
-        Войти
-      </Button>
+      <form action="" ref={formRef} onSubmit={(e) => login(e)}>
+        <Input
+          value={inputEmailValue}
+          name="email"
+          placeholder="E-mail"
+          type="text"
+          extraClass={`${styles.input}`}
+          error={loginRequestError}
+          errorText={"Проверьте введённые данные"}
+          onChange={(e) => setInputEmailValue(e.target.value)}
+        />
+        <Input
+          value={inputPasswordValue}
+          name="password"
+          extraClass={`${styles.input}`}
+          placeholder="Пароль"
+          type={!showPassword ? "password" : "text"}
+          icon={!showPassword ? "ShowIcon" : "HideIcon"}
+          onIconClick={() => setShowPassword(!showPassword)}
+          error={loginRequestError}
+          errorText={"Проверьте введённые данные"}
+          onChange={(e) => setInputPasswordValue(e.target.value)}
+        />
+        <Button
+          htmlType="submit"
+          type="primary"
+          size="medium"
+          extraClass="mt-6"
+        >
+          Войти
+        </Button>
+      </form>
       <p className="mt-20">
         <span className="text text_type_main-default">
           Вы — новый пользователь?
