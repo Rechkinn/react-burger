a7a0eaaa86ac3b812196497e39ef9df8e89eb21b
Rechkinn <al.red197218@gmail.com>
2025-10-28 15:32:58 +0700

    Добавлено сохранение id ингредиента в localStorage для ситуации с перезагрузкой страницы

--- src/services/reducers/ingredient-details.js
+++ src/services/reducers/ingredient-details.js
@@ -10,11 +10,14 @@ const initialState = {
 export const ingredientDetailsReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_INGREDIENT_DETAILS:
+      localStorage.setItem("ingredientDetails", action.ingredientDetails._id);
+      // localStorage.setItem("ingredientDetails", "fwef");
       return {
         ...state,
         ingredientDetails: action.ingredientDetails,
       };
     case REMOVE_INGREDIENT_DETAILS:
+      localStorage.removeItem("ingredientDetails");
       return {
         ...state,
         ingredientDetails: {},
