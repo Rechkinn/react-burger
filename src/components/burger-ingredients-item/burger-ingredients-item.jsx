a9fe3e8a2c9d223f04caf342a2045b32e99bf3a7
Rechkinn <al.red197218@gmail.com>
2025-10-28 15:29:03 +0700

    Убраны не нужные пропсы

--- src/components/burger-ingredients-item/burger-ingredients-item.jsx
+++ src/components/burger-ingredients-item/burger-ingredients-item.jsx
@@ -5,47 +5,41 @@ import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
 import { forwardRef } from "react";
 
-const BurgerIngredientsItem = forwardRef(
-  ({ type, openModalWithIngredientDetails }, ref) => {
-    const { burgerIngredients } = useSelector(
-      (store) => store.burgerIngredients
-    );
+const BurgerIngredientsItem = forwardRef(({ type }, ref) => {
+  const { burgerIngredients } = useSelector((store) => store.burgerIngredients);
 
-    function getIngredientsFromType(typeItem) {
-      return burgerIngredients.filter(
-        (ingredient) => ingredient.type === typeItem
-      );
-    }
-    function translateTypeToRussianLanguage(type) {
-      if (type === BUN) return "Булки";
-      else if (type === SAUCE) return "Соусы";
-      else if (type === MAIN) return "Котлеты";
-    }
-
-    return (
-      <section ref={ref} className={`mt-10 ${styles.item}`}>
-        <h2 className="pl-5 pr-5 text text_type_main-medium">
-          {translateTypeToRussianLanguage(type)}
-        </h2>
-        <div className={`pt-6 ${styles.cards}`}>
-          {getIngredientsFromType(type).map((ingredient) => {
-            return (
-              <BurgerIngredientsCard
-                key={ingredient._id}
-                typeDrag={ingredient.type === BUN ? BUN : "ingredient"}
-                ingredient={ingredient}
-                openModalWithIngredientDetails={openModalWithIngredientDetails}
-              />
-            );
-          })}
-        </div>
-      </section>
+  function getIngredientsFromType(typeItem) {
+    return burgerIngredients.filter(
+      (ingredient) => ingredient.type === typeItem
     );
   }
-);
+  function translateTypeToRussianLanguage(type) {
+    if (type === BUN) return "Булки";
+    else if (type === SAUCE) return "Соусы";
+    else if (type === MAIN) return "Котлеты";
+  }
+
+  return (
+    <section ref={ref} className={`mt-10 ${styles.item}`}>
+      <h2 className="pl-5 pr-5 text text_type_main-medium">
+        {translateTypeToRussianLanguage(type)}
+      </h2>
+      <div className={`pt-6 ${styles.cards}`}>
+        {getIngredientsFromType(type).map((ingredient) => {
+          return (
+            <BurgerIngredientsCard
+              key={ingredient._id}
+              typeDrag={ingredient.type === BUN ? BUN : "ingredient"}
+              ingredient={ingredient}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
+});
 export default BurgerIngredientsItem;
 
 BurgerIngredientsItem.propTypes = {
   type: PropTypes.string.isRequired,
-  openModalWithIngredientDetails: PropTypes.func.isRequired,
 };
