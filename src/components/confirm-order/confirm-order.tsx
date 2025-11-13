ccf19464152fadb2b5e843a8b7c862ef484c8e71
Aleksey Redkin <al.red197218@gmail.com>
2025-11-13 19:01:29 +0700

    Типизация компонента ConfirmOrder с помощью TypeScript

--- src/components/confirm-order/confirm-order.tsx
+++ src/components/confirm-order/confirm-order.tsx
@@ -9,19 +9,45 @@ import OrderDetails from "../order-details/order-details";
 import { useModal } from "../../hooks/useModal";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
+import { FC, ReactNode } from "react";
+import { TIngredient } from "../../utils/types";
 
-function ConfirmOrder({ section, ...props }) {
+type TSize = "small" | "medium" | "large" | undefined;
+
+type TConfirmOrderProps = {
+  section: string;
+  className: string;
+  onlyButton?: boolean;
+  size: TSize;
+  textButton: string;
+};
+
+const ConfirmOrder: FC<TConfirmOrderProps> = ({
+  section,
+  className,
+  onlyButton,
+  size,
+  textButton,
+}) => {
   const navigate = useNavigate();
   const { isModalOpen, openModal, closeModal } = useModal();
-  const { user } = useSelector((store) => store.userData);
+  const { user } = useSelector((store: any) => store.userData);
   const { bun, burgerConstructor } = useSelector(
-    (store) => store.burgerConstructor
+    (store: any) => store.burgerConstructor
   );
 
-  function calculatePrice() {
-    const priceWithoutBuns = burgerConstructor.reduce((sum, ingredient) => {
-      return (sum += ingredient.ingredient.price);
-    }, 0);
+  function calculatePrice(): ReactNode {
+    const priceWithoutBuns = burgerConstructor.reduce(
+      (
+        sum: number,
+        ingredient: { ingredient: TIngredient; uniqueId: string }
+      ) => {
+        console.log("ingredient");
+        console.log(ingredient);
+        return (sum += ingredient.ingredient.price);
+      },
+      0
+    );
 
     return bun?.ingredient
       ? bun.ingredient.price * 2 + priceWithoutBuns
@@ -31,52 +57,40 @@ function ConfirmOrder({ section, ...props }) {
   return (
     <>
       {isModalOpen && (
-        <Modal
-          type={"OrderDetails"}
-          functionToClose={closeModal}
-          indents={"mt-5 mb-4"}
-        >
+        <Modal functionToClose={closeModal} indents={"mt-5 mb-4"}>
           <OrderDetails />
         </Modal>
       )}
 
-      <div className={`mt-10 ${styles.confirmOrder} ${props.className}`}>
-        {!props.onlyButton && (
+      <div className={`mt-10 ${styles.confirmOrder} ${className}`}>
+        {!onlyButton && (
           <div className={`mr-10 ${styles.price}`}>
             <span className={`mr-2 text text_type_digits-medium`}>
               {calculatePrice()}
             </span>
-            <CurrencyIcon />
+            <CurrencyIcon type="primary" />
           </div>
         )}
         <Button
           disabled={burgerConstructor.length > 0 && bun ? false : true}
           htmlType="button"
           type="primary"
-          size={props.size}
+          size={size}
           onClick={
             user
               ? section === "BurgerConstructor"
                 ? openModal
-                : null
+                : undefined
               : () => {
                   navigate("/login");
                 }
           }
         >
-          {props.textButton}
+          {textButton}
         </Button>
       </div>
     </>
   );
-}
+};
 
 export default ConfirmOrder;
-
-ConfirmOrder.propTypes = {
-  className: PropTypes.string.isRequired,
-  onlyButton: PropTypes.bool,
-  size: PropTypes.string.isRequired,
-  textButton: PropTypes.string.isRequired,
-  section: PropTypes.string.isRequired,
-};
