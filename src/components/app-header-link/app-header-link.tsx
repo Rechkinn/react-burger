f60e29d008ed7904160884f6edbdac988dc0a711
Rechkinn <al.red197218@gmail.com>
2025-11-14 19:55:36 +0700

    Типизация компонента AppHeaderLink с помощью TypeScript

--- src/components/app-header-link/app-header-link.tsx
+++ src/components/app-header-link/app-header-link.tsx
@@ -1,21 +1,28 @@
 import { NavLink } from "react-router";
 import styles from "./app-header-link.module.css";
+import { FC, PropsWithChildren } from "react";
 
-function AppHeaderLink({ ...props }) {
-  const paddings = "pl-5 pr-5 pt-4 pb-4 ";
+type TAppHeaderLinkProps = {
+  to: string;
+  onClick?: () => void;
+} & PropsWithChildren;
+
+const AppHeaderLink: FC<TAppHeaderLinkProps> = ({ to, onClick, children }) => {
+  const paddings: string = "pl-5 pr-5 pt-4 pb-4 ";
 
   return (
     <NavLink
+      to={to}
       className={({ isActive }) => {
         return isActive
           ? `${paddings} ${styles.activeLink}`
           : `text_color_inactive ${paddings} ${styles.link}`;
       }}
-      {...props}
+      onClick={onClick}
     >
-      {props.children}
+      {children}
     </NavLink>
   );
-}
+};
 
 export default AppHeaderLink;
