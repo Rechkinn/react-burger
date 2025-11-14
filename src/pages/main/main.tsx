ff9752be03cd9e84138d7f8b9ecc06e877e304d9
Rechkinn <al.red197218@gmail.com>
2025-11-14 20:04:18 +0700

    Типизация компонента Main с помощью TypeScript

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import { FC, useState } from "react";
 import styles from "./main.module.css";
 import BurgerIngredients from "../../components/burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../../components/burger-constructor/burger-constructor";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
-export default function Main() {
-  const [activeSection, setActiveSection] = useState("BurgerConstructor");
+const Main: FC = () => {
+  const [activeSection, setActiveSection] =
+    useState<string>("BurgerConstructor");
   return (
     <main className={styles.main}>
       <DndProvider backend={HTML5Backend}>
@@ -33,4 +34,6 @@ export default function Main() {
       </DndProvider>
     </main>
   );
-}
+};
+
+export default Main;
