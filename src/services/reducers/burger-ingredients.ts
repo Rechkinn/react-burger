d236758cc3d806379ea69be190360a218c564e3f
Rechkinn <al.red197218@gmail.com>
2025-12-01 21:19:04 +0700

    Типизация редьюсера для BurgerIngredients

--- src/services/reducers/burger-ingredients.ts
+++ src/services/reducers/burger-ingredients.ts
@@ -1,16 +1,27 @@
+import { TIngredient } from "../../utils/types";
 import {
   GET_BURGER_INGREDIENTS_REQUEST,
   GET_BURGER_INGREDIENTS_REQUEST_SUCCESS,
   GET_BURGER_INGREDIENTS_REQUEST_ERROR,
+  TBurgerIngredientsActions,
 } from "../actions/burger-ingredients";
 
-const initialState = {
+type TBurgerIngredientsState = {
+  burgerIngredients: TIngredient[];
+  burgerIngredientsRequest: boolean;
+  burgerIngredientsRequestFailed: boolean;
+};
+
+const initialState: TBurgerIngredientsState = {
   burgerIngredients: [],
   burgerIngredientsRequest: false,
   burgerIngredientsRequestFailed: false,
 };
 
-export const burgerIngredientsReducer = (state = initialState, action) => {
+export const burgerIngredientsReducer = (
+  state = initialState,
+  action: TBurgerIngredientsActions
+): TBurgerIngredientsState => {
   switch (action.type) {
     case GET_BURGER_INGREDIENTS_REQUEST:
       return {
