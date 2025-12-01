235af2e73eb16b2ebed8d7fa08ce2f083ffe10e6
Rechkinn <al.red197218@gmail.com>
2025-12-01 21:11:58 +0700

    Убрана логика добавления и удаление деталей заказа

--- src/pages/ingredient-details-page/ingredient-details-page.tsx
+++ src/pages/ingredient-details-page/ingredient-details-page.tsx
@@ -1,43 +1,15 @@
 import styles from "./ingredient-details-page.module.css";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "../../utils/additionalStorageTyping";
 import { useParams } from "react-router";
 import IngredientDetails from "../../components/ingredient-details/ingredient-details";
-import {
-  ADD_INGREDIENT_DETAILS,
-  REMOVE_INGREDIENT_DETAILS,
-} from "../../services/actions/ingredient-details";
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import NotFound from "../not-found/not-found";
 
 const IngredientDetailsPage: FC = () => {
-  const dispatch = useDispatch();
   const { id } = useParams<string>();
-  const { burgerIngredients } = useSelector(
-    (store: any) => store.burgerIngredients
-  );
+  const { burgerIngredients } = useSelector((store) => store.burgerIngredients);
 
-  useEffect(() => {
-    return () => {
-      dispatch({
-        type: REMOVE_INGREDIENT_DETAILS,
-      });
-    };
-  }, [dispatch]);
-
-  function tryGetIngredient(): boolean {
-    for (let i = 0; i < burgerIngredients.length; i++) {
-      if (burgerIngredients[i]._id === id) {
-        dispatch({
-          type: ADD_INGREDIENT_DETAILS,
-          ingredientDetails: burgerIngredients[i],
-        });
-        return true;
-      }
-    }
-    return false;
-  }
-
-  return tryGetIngredient() ? (
+  return burgerIngredients.find((ingredient) => ingredient._id === id) ? (
     <div className={styles.container}>
       <h1 className="text text_type_main-large">Детали ингредиента</h1>
       <IngredientDetails />
