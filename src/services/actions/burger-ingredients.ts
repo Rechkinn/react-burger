8a0207964ffa819f212938bf8186b23dd00f9b7b
Rechkinn <al.red197218@gmail.com>
2025-12-01 21:15:12 +0700

    Типизация экшенов для BurgerIngredients

--- src/services/actions/burger-ingredients.ts
+++ src/services/actions/burger-ingredients.ts
@@ -1,13 +1,32 @@
+import { AppDispatch, AppThunk } from "../../utils/additionalStorageTyping";
 import { doRequest } from "../../utils/doRequest";
+import { TIngredient } from "../../utils/types";
 
-export const GET_BURGER_INGREDIENTS_REQUEST = "GET_BURGER_INGREDIENTS";
-export const GET_BURGER_INGREDIENTS_REQUEST_SUCCESS =
+export const GET_BURGER_INGREDIENTS_REQUEST: "GET_BURGER_INGREDIENTS_REQUEST" =
+  "GET_BURGER_INGREDIENTS_REQUEST";
+export const GET_BURGER_INGREDIENTS_REQUEST_SUCCESS: "GET_BURGER_INGREDIENTS_REQUEST_SUCCESS" =
   "GET_BURGER_INGREDIENTS_REQUEST_SUCCESS";
-export const GET_BURGER_INGREDIENTS_REQUEST_ERROR =
+export const GET_BURGER_INGREDIENTS_REQUEST_ERROR: "GET_BURGER_INGREDIENTS_REQUEST_ERROR" =
   "GET_BURGER_INGREDIENTS_REQUEST_ERROR";
 
-export function getBurgerIngredients(): any {
-  return function (dispatch: any) {
+export interface IGetBurgerIngredientsRequestAction {
+  readonly type: typeof GET_BURGER_INGREDIENTS_REQUEST;
+}
+export interface IGetBurgerIngredientsRequestSuccessAction {
+  readonly type: typeof GET_BURGER_INGREDIENTS_REQUEST_SUCCESS;
+  readonly burgerIngredients: TIngredient[];
+}
+export interface IGetBurgerIngredientsRequestErrorAction {
+  readonly type: typeof GET_BURGER_INGREDIENTS_REQUEST_ERROR;
+}
+
+export type TBurgerIngredientsActions =
+  | IGetBurgerIngredientsRequestAction
+  | IGetBurgerIngredientsRequestSuccessAction
+  | IGetBurgerIngredientsRequestErrorAction;
+
+export const getBurgerIngredients: AppThunk = () => {
+  return function (dispatch: AppDispatch) {
     dispatch({
       type: GET_BURGER_INGREDIENTS_REQUEST,
     });
@@ -25,4 +44,4 @@ export function getBurgerIngredients(): any {
         })
       );
   };
-}
+};
