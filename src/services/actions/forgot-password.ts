d2cc7076bbf2abe314da90921159779a4a4c2623
Rechkinn <al.red197218@gmail.com>
2025-12-01 21:15:51 +0700

    Типизация экшенов для ForgotPassword

--- src/services/actions/forgot-password.ts
+++ src/services/actions/forgot-password.ts
@@ -1,13 +1,31 @@
+import { AppDispatch, AppThunk } from "../../utils/additionalStorageTyping";
 import { EMethod } from "../../utils/consts";
 import { doRequest } from "../../utils/doRequest";
 
-export const FORGOT_PASSWORD_REQUEST = "FORGOT_PASSWORD_REQUEST";
-export const FORGOT_PASSWORD_REQUEST_ERROR = "FORGOT_PASSWORD_REQUEST_ERROR";
-export const FORGOT_PASSWORD_REQUEST_SUCCESS =
+export const FORGOT_PASSWORD_REQUEST: "FORGOT_PASSWORD_REQUEST" =
+  "FORGOT_PASSWORD_REQUEST";
+export const FORGOT_PASSWORD_REQUEST_ERROR: "FORGOT_PASSWORD_REQUEST_ERROR" =
+  "FORGOT_PASSWORD_REQUEST_ERROR";
+export const FORGOT_PASSWORD_REQUEST_SUCCESS: "FORGOT_PASSWORD_REQUEST_SUCCESS" =
   "FORGOT_PASSWORD_REQUEST_SUCCESS";
 
-export const doForgotPassword = (userData: any): any => {
-  return function (dispatch: any) {
+export interface IForgotPasswordRequestAction {
+  readonly type: typeof FORGOT_PASSWORD_REQUEST;
+}
+export interface IForgotPasswordRequestErrorAction {
+  readonly type: typeof FORGOT_PASSWORD_REQUEST_ERROR;
+}
+export interface IForgotPasswordRequestSuccessAction {
+  readonly type: typeof FORGOT_PASSWORD_REQUEST_SUCCESS;
+}
+
+export type TForgotPasswordActions =
+  | IForgotPasswordRequestAction
+  | IForgotPasswordRequestErrorAction
+  | IForgotPasswordRequestSuccessAction;
+
+export const doForgotPassword: AppThunk = (userData) => {
+  return function (dispatch: AppDispatch) {
     dispatch({
       type: FORGOT_PASSWORD_REQUEST,
     });
