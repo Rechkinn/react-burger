e05d58955756e3d262b9ce007c5fc6cdce90cf6c
Rechkinn <al.red197218@gmail.com>
2025-12-01 21:16:27 +0700

    Типизация экшенов для Logout

--- src/services/actions/logout.ts
+++ src/services/actions/logout.ts
@@ -1,14 +1,32 @@
+import { AppDispatch, AppThunk } from "../../utils/additionalStorageTyping";
 import { EMethod } from "../../utils/consts";
 import { doRequest } from "../../utils/doRequest";
 import { REMOVE_ACCESS_TOKEN, REMOVE_REFRESH_TOKEN } from "./token";
 import { USER_REMOVE_DATA } from "./user-data";
 
-export const LOGOUT_REQUEST = "LOGOUT_REQUEST";
-export const LOGOUT_REQUEST_ERROR = "LOGOUT_REQUEST_ERROR";
-export const LOGOUT_REQUEST_SUCCESS = "LOGOUT_REQUEST_SUCCESS";
+export const LOGOUT_REQUEST: "LOGOUT_REQUEST" = "LOGOUT_REQUEST";
+export const LOGOUT_REQUEST_ERROR: "LOGOUT_REQUEST_ERROR" =
+  "LOGOUT_REQUEST_ERROR";
+export const LOGOUT_REQUEST_SUCCESS: "LOGOUT_REQUEST_SUCCESS" =
+  "LOGOUT_REQUEST_SUCCESS";
 
-export const doLogout = (): any => {
-  return function (dispatch: any) {
+export interface ILogoutRequestAction {
+  readonly type: typeof LOGOUT_REQUEST;
+}
+export interface ILogoutRequestErrorAction {
+  readonly type: typeof LOGOUT_REQUEST_ERROR;
+}
+export interface ILogoutRequestSuccessAction {
+  readonly type: typeof LOGOUT_REQUEST_SUCCESS;
+}
+
+export type TLogoutActions =
+  | ILogoutRequestAction
+  | ILogoutRequestErrorAction
+  | ILogoutRequestSuccessAction;
+
+export const doLogout: AppThunk = () => {
+  return function (dispatch: AppDispatch) {
     dispatch({
       type: LOGOUT_REQUEST,
     });
