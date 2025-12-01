4c651cb1fc0fed755470c510a313517e4198402a
Rechkinn <al.red197218@gmail.com>
2025-12-01 21:17:01 +0700

    Типизация экшенов для ResetPassword

--- src/services/actions/reset-password.ts
+++ src/services/actions/reset-password.ts
@@ -1,12 +1,31 @@
+import { AppDispatch, AppThunk } from "../../utils/additionalStorageTyping";
 import { EMethod } from "../../utils/consts";
 import { doRequest } from "../../utils/doRequest";
 
-export const RESET_PASSWORD_REQUEST = "RESET_PASSWORD_REQUEST";
-export const RESET_PASSWORD_REQUEST_ERROR = "RESET_PASSWORD_REQUEST_ERROR";
-export const RESET_PASSWORD_REQUEST_SUCCESS = "RESET_PASSWORD_REQUEST_SUCCESS";
+export const RESET_PASSWORD_REQUEST: "RESET_PASSWORD_REQUEST" =
+  "RESET_PASSWORD_REQUEST";
+export const RESET_PASSWORD_REQUEST_ERROR: "RESET_PASSWORD_REQUEST_ERROR" =
+  "RESET_PASSWORD_REQUEST_ERROR";
+export const RESET_PASSWORD_REQUEST_SUCCESS: "RESET_PASSWORD_REQUEST_SUCCESS" =
+  "RESET_PASSWORD_REQUEST_SUCCESS";
 
-export const doResetPassword = (userData: any): any => {
-  return function (dispatch: any) {
+export interface IResetPasswordRequestAction {
+  readonly type: typeof RESET_PASSWORD_REQUEST;
+}
+export interface IResetPasswordRequestErrorAction {
+  readonly type: typeof RESET_PASSWORD_REQUEST_ERROR;
+}
+export interface IResetPasswordRequestSuccessAction {
+  readonly type: typeof RESET_PASSWORD_REQUEST_SUCCESS;
+}
+
+export type TResetPasswordActions =
+  | IResetPasswordRequestAction
+  | IResetPasswordRequestErrorAction
+  | IResetPasswordRequestSuccessAction;
+
+export const doResetPassword: AppThunk = (userData) => {
+  return function (dispatch: AppDispatch) {
     dispatch({
       type: RESET_PASSWORD_REQUEST,
     });
